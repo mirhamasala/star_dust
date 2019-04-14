87ac24eedd2d0f1ed2bdbe65fac2c2b8aa3ff89e
Mirha Masala <mirha.masala@me.com>
2019-04-14 18:02:21 +0800

    Move star to click position - 2hr mark

--- all.js
+++ all.js
@@ -1,9 +1,21 @@
+const star = document.querySelector('.fa-star');
 const sky = document.querySelector('.sky');
 
-document.body.addEventListener('click', addStar);
+sky.addEventListener('click', handleClick, false);
 
-function addStar(event) {
-    const star = document.createElement('i');
-    star.setAttribute('class', 'fa fa-star');
-    sky.appendChild(star);
+function handleClick(event) {
+    star.classList.remove('glow');
+    moveStar(event);
+    addGlow();
+}
+
+function moveStar(event) {
+    let xPosition = event.clientX - (star.offsetWidth / 2);
+    let yPosition = event.clientY - (star.offsetHeight / 2);
+    const translate3DValue = `translate3d(${xPosition}px, ${yPosition}px, 0)`;
+    star.style.transform = translate3DValue;
+}
+
+function addGlow() {
+    star.classList.add('glow');
 }

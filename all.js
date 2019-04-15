e5efd437b6d7688a19c1ec73533fde2e6580679f
Mirha Masala <mirha.masala@me.com>
2019-04-15 13:48:33 +0800

    Refactor JS

--- all.js
+++ all.js
@@ -12,8 +12,7 @@ function handleClick(event) {
 function moveStar(event) {
     let xPosition = event.clientX - (star.offsetWidth / 2);
     let yPosition = event.clientY - (star.offsetHeight / 2);
-    const translate3DValue = `translate3d(${xPosition}px, ${yPosition}px, 0)`;
-    star.style.transform = translate3DValue;
+    star.style.transform = `translate3d(${xPosition}px, ${yPosition}px, 0)`;
 }
 
 function addGlow() {
